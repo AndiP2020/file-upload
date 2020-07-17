e044616a33d8c12699861f040d50a43df61b8a43
Petkau, Andreas <apetkau@deloitte.de>
2020-07-17 16:51:51 +0200

    Code runs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,20 +15,16 @@ export class AppComponent {
     this.selectedFile = <File>event.target.files[0];    
   }
 
+  ngOnInit() {
+    this.http.get('http://localhost:5000/Weatherforecast').subscribe(event => {
+        console.log("happiness");
+      });  
+  }
   onUpload() {
+    console.log("onUpload()");
     const fd = new FormData();
     fd.append('image', this.selectedFile, this.selectedFile.name)
-    this.http.post('', fd, {
-      reportProgress: true,
-      observe: 'events'
-    })     
-      .subscribe(event => {
-        if (event.type === HttpEventType.UploadProgress) {
-          console.log('Upload Progress: ' + Math.round(event.loaded / event.total * 100) + '%')
-        } else if (event.type === HttpEventType.Response) {
-          console.log(event);
-        }        
-      });    
+      
   }
 }
 
